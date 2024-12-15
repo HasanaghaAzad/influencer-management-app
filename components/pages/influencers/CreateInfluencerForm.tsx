ad40ac225f47ff95cf5675b9398d559bf885b810
HasanaghaAzad <hasanaghaazad@gmail.com>
2024-12-16 01:44:00 +0200

    reverted back PascalCase files

---
+++ components/pages/influencers/CreateInfluencerForm.tsx
@@ -0,0 +1,98 @@
+"use client";
+import Input from "@/components/ui/form/Input";
+import Textarea from "@/components/ui/form/Textarea";
+import React, { useActionState, useEffect, useState } from "react";
+import { ManagerSelect } from "./ManagerSelect";
+import {
+  getAllUsers,
+  getCurrentUser,
+} from "@/app/services/frontend/userService";
+import { create } from "@/app/actions/influencers/actions";
+import { CreationResponse } from "@/app/types/influencers";
+import { AllUsersList } from "@/app/types/users";
+import { SubmitButton } from "@/components/ui/form/SubmitButton";
+
+const initialState: CreationResponse = {};
+
+export default function CreateInfluencerForm() {
+  const [state, formAction, pending] = useActionState(create, initialState);
+
+  const [allManagers, setManagers] = useState([] as AllUsersList);
+
+  const [preSelectedManager, setPreSelectedManager] = useState<string>();
+
+  useEffect(() => {
+    (async () => {
+      const allUsers = await getAllUsers();
+      setManagers(allUsers);
+
+      const currentUser = await getCurrentUser();
+      setPreSelectedManager(currentUser.id.toString());
+    })();
+  }, []);
+  return (
+    <>
+      <h2 className="text-xl font-bold tracking-tight text-zinc-700 my-3">
+        Create new Influencer
+      </h2>
+
+      {state.success && (
+        <div className="mb-4 p-4 text-sm text-green-800 bg-green-100 border border-green-200 rounded-lg">
+          Successfully created!
+        </div>
+      )}
+
+      {state.error && (
+        <div className="mb-4 p-4 text-sm text-red-800 bg-red-100 border border-red-200 rounded-lg">
+          An error occurred: {state.error || "Please try again."}
+        </div>
+      )}
+
+      <form className="space-y-4" action={formAction}>
+        <Input
+          name="firstName"
+          defaultValue={state?.values?.firstName}
+          label={{ labelText: "First name" }}
+          required
+          error={state?.errors?.firstName?.join("<br>")}
+        />
+
+        <Input
+          name="lastName"
+          defaultValue={state?.values?.lastName}
+          label={{ labelText: "Last name" }}
+          required
+          error={state?.errors?.lastName?.join("<br>")}
+        />
+
+        <Textarea
+          name="instagram"
+          value={state?.values?.instagram}
+          label={{ labelText: "Instagram accounts" }}
+          required
+          helperNote="Each username on a new line."
+          error={state?.errors?.instagram?.join("--")}
+        />
+
+        <Textarea
+          name="tiktok"
+          value={state?.values?.tiktok}
+          label={{ labelText: "Tiktok accounts" }}
+          required
+          helperNote="Each username on a new line."
+          error={state?.errors?.tiktok?.join("<br>")}
+        />
+
+        <ManagerSelect
+          allManagers={allManagers}
+          selectName="managerId"
+          label={{ labelText: "Manager", isVisible: true }}
+          defaultValue={state?.values?.managerId || preSelectedManager}
+          error={state?.errors?.managerId?.join("<br>")}
+        />
+
+        <SubmitButton pending={pending}>Save</SubmitButton>
+      </form>
+    </>
+  );
+}
